695e572f6b283307d8bf8e45536409377e9a74eb
yohanderose <yohanderose@gmail.com>
2018-12-03 08:29:49 +1300

    initial commit

---
+++ pong.py
@@ -0,0 +1,17 @@
+import turtle as tt
+
+
+def main():
+    
+    print("Starting Pong...")
+    wn = tt.Turtle()
+
+    wn.penup()
+    wn.shape("circle")
+    tt.update()
+
+    tt.done()
+
+if __name__ == "__main__":
+    main()
+
