fe0fdfe9e20e83f25f580468009a3bf792587014
Anthony Dickson <eight0153@gmail.com>
2018-12-04 08:03:34 +1300

    add bounding boxes

--- pong.py
+++ pong.py
@@ -44,61 +44,56 @@ class BoundingBox:
         return Point(self.origin.x + self.width, self.origin.y + self.height)
 
     def intersects(self, other):
-        assert other is BoundingBox
+        assert isinstance(other, BoundingBox)
 
-        if self.top_left().x < other.bottom_right().x and self.top_left().y < other.bottom_right().y:
-            return True
+        x_overlaps =  self.top_right().x > other.top_left().x and other.top_right().x > self.top_left().x
+        y_overlaps = self.bottom_left().y > other.top_left().y and other.bottom_left().y > self.top_left().y
 
-        if self.top_right().x > other.bottom_left().x and self.top_right().y < other.bottom_left().y:
-            return True
+        return x_overlaps and y_overlaps
 
-        if self.bottom_left().x < other.top_right().x and self.bottom_left().y > other.top_right().y:
-            return True
-
-        if self.bottom_right().x > other.top_left().x and self.bottom_right().y > other.top_left().y:
-            return True
-
-        return False
         
         
 class GameObject:
     def __init__(self, canvas, x1, y1, x2, y2, velocity=Point(0, 0)):
         self.canvas = canvas
-        self.canvas_object = canvas.create_rectangle(x1, y1, x2, y2, fill="black")
+        self.canvas_object = None #canvas.create_rectangle(x1, y1, x2, y2, fill="black")
         self.bounding_box = BoundingBox(Point(x1, y1), x2 - x1, y2 - y1)
         self.velocity = velocity
 
-    def move(self):
-        self.bounding_box.origin + self.velocity
+    def update(self):
+        self.bounding_box.origin += self.velocity
         self.canvas.move(self.canvas_object, self.velocity.x, self.velocity.y)
 
     def get_position(self):
-        return self.canvas.coords(self.canvas_object)    
+        return self.canvas.coords(self.canvas_object)
+
+    def intersects(self, other):
+        assert isinstance(other, GameObject)
+
+        return self.bounding_box.intersects(other.bounding_box)
 
 class Paddle(GameObject):
+
     PLAYER_ONE = 1
     PLAYER_TWO = 2
 
-    def __init__(self, canvas, player, speed=20):
-        if player == Paddle.PLAYER_ONE:
-            x1 = 25
-            y1 = 180
-            x2 = 35
-            y2 = 220
-        else:
-            x1 = 765
-            y1 = 180
-            x2 = 775
-            y2 = 220
-        
+    def __init__(self, canvas, x1, y1, x2, y2, speed=10):
         super().__init__(canvas, x1, y1, x2, y2)
         self.canvas_object = canvas.create_rectangle(x1, y1, x2, y2, fill="black")  
-        self.speed = speed      
+        self.speed = speed
 
-    def move_up(self, event):
+    @staticmethod
+    def get_player(canvas, player, speed=10):
+        """Factory method for creating a player paddle."""
+        if player == Paddle.PLAYER_ONE:
+            return Paddle(canvas, 25, 180, 35, 220, speed)
+        else:
+            return Paddle(canvas, 765, 180, 775, 220, speed)
+
+    def move_up(self):
         self.velocity = Point(0, -self.speed)
 
-    def move_down(self, event):
+    def move_down(self):
         self.velocity = Point(0, self.speed)
 
 class Ball(GameObject):
@@ -115,32 +110,32 @@ class Manager(object):
         self.window = window
         self.canvas = canvas
         self.ball = Ball(self.canvas, 100, 100, 112, 112, speed=3)
-        self.pad1 = Paddle(self.canvas, Paddle.PLAYER_ONE)
-        self.pad2 = Paddle(self.canvas, Paddle.PLAYER_TWO)
+        self.pad1 = Paddle.get_player(self.canvas, Paddle.PLAYER_ONE)
+        self.pad2 = Paddle.get_player(self.canvas, Paddle.PLAYER_TWO)
 
-        window.bind('<KeyPress>', self.handle_key_pressed)
-        window.bind('<KeyRelease>', self.handle_key_released)
+        window.bind('<KeyPress>', lambda event: self.handle_key_pressed(event))
+        window.bind('<KeyRelease>', lambda event: self.handle_key_released(event))
 
     def handle_key_pressed(self, event):
-        print('%s pressed' % event.char)
+        key = event.keysym.lower()
 
-        if event.char == 'w':
+        if key == 'w':
             self.pad1.move_up()
-        elif event.char == 's':
+        elif key == 's':
             self.pad1.move_down()
 
-        if event.char == 'up':
+        if key == 'up':
             self.pad2.move_up()            
-        elif event.char == 'down':
+        elif key == 'down':
             self.pad2.move_down()
         
     def handle_key_released(self, event):
-        print('%s released' % event.char)
-
-        if event.char == 'w' or event.char == 's':
+        key = event.keysym.lower()
+        
+        if key == 'w' or key == 's':
             self.pad1.velocity = Point()
 
-        if event.char == 'up' or event.char == 'down':
+        if key == 'up' or key == 'down':
             self.pad2.velocity = Point()
 
     def physics(self):
@@ -149,12 +144,11 @@ class Manager(object):
         pad2_x1, pad2_y1, pad2_x2, pad2_y2 = self.pad2.get_position()
 
     # Check if ball has collided with a paddle.
-        if (pad1_x1 <= x1 <= pad1_x2 and pad1_y1 <= y1 + ((y2-y1)/2) <= pad1_y2):
+        if self.ball.intersects(self.pad1) or self.ball.intersects(self.pad2):
             self.ball.velocity.x *= -1
 
         # TODO: Second paddle
         # TODO: Bounding box
-        # TODO: Paddle controls
         
     # Check if ball past upper or lower screen bounds. 
         if y1 <= 0 or y2 >= 400:
@@ -163,13 +157,19 @@ class Manager(object):
         if x1 <= 0 or x2 >= 800:
              self.ball.velocity.x *= -1
 
+        if pad1_y1 < 0 or pad2_y2 > 400:
+            self.pad1.velocity = Point()
+
+        if pad2_y1 < 0 or pad2_y2 > 400:
+            self.pad2.velocity = Point()
+
         
 
     def run(self):
+        self.ball.update()
+        self.pad1.update()
+        self.pad2.update()
         self.physics()
-        self.ball.move()
-        self.pad1.move()
-        self.pad2.move()
         self.canvas.after(16, self.run)
         
 
